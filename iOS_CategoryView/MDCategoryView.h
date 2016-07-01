789f76fa0f95fe666c15f1d29c1a352cf394fff9
刘隆昌 <liulongchang@langyue.org>
2016-07-01 20:06:33 +0800

    first commit

---
+++ iOS_CategoryView/MDCategoryView.h
@@ -0,0 +1,13 @@
+//
+//  MDCategoryView.h
+//  iOS_CategoryView
+//
+//  Created by langyue on 16/7/1.
+//  Copyright © 2016年 langyue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MDCategoryView : UIView
+
+@end
---
+++ iOS_CategoryView/MDCategoryView.m
@@ -0,0 +1,21 @@
+//
+//  MDCategoryView.m
+//  iOS_CategoryView
+//
+//  Created by langyue on 16/7/1.
+//  Copyright © 2016年 langyue. All rights reserved.
+//
+
+#import "MDCategoryView.h"
+
+@implementation MDCategoryView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
