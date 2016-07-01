83d1e34802727269c23eb8a466179eea45638d96
刘隆昌 <liulongchang@langyue.org>
2016-07-01 12:05:39 +0800

    Initial Commit

---
+++ iOS_CategoryView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS_CategoryView
+//
+//  Created by langyue on 16/7/1.
+//  Copyright © 2016年 langyue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS_CategoryView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS_CategoryView
+//
+//  Created by langyue on 16/7/1.
+//  Copyright © 2016年 langyue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
